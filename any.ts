b65ba0f4ce7575dea5177beac88f1a4775297fa3
Will Smart <will@melonhealth.com>
2019-09-17 08:50:17 +1200

    Add interfaces from the code-snippet repo

---
+++ any.ts
@@ -0,0 +1,2 @@
+export type anyValue = object | number | string | boolean | symbol | null | undefined;
+export type anyObject = { [key: string]: anyValue };
---
+++ code-snippet.ts
@@ -0,0 +1,19 @@
+// TODO document
+
+import { anyValue } from "../any";
+
+export interface CodeSnippetCallInstance {
+  retryAfterPromise(promise: Promise<void>, name: string): void;
+  registerSideEffect(perform: CodeSnippetSideEffect, name?: string): void;
+}
+
+export interface CodeSnippetArg {
+  value: anyValue;
+  valueGetter?: (callInstance: CodeSnippetCallInstance) => anyValue;
+  defaultValueForKeyPath: (keyPath: string) => anyValue;
+}
+
+export type CodeSnippetSideEffect = { run: () => Promise<void> | undefined; name?: string };
+export type CodeSnippetCallSuccess = { result?: anyValue; sideEffects: CodeSnippetSideEffect[] };
+export type CodeSnippetCallRetry = { retryingAfterPromises?: string[] };
+export type CodeSnippetCallResult = CodeSnippetCallSuccess | CodeSnippetCallRetry;
