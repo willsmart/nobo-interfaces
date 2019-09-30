337bff21b46af02d8d16324c41ce1a35ec64b82e
Will Smart <will@melonhealth.com>
2019-09-30 23:51:49 +1300

    Add `anyPrimitive`

--- any.ts
+++ any.ts
@@ -1,3 +1,4 @@
-export type anyValue = object | number | string | boolean | symbol | null | undefined;
+export type anyPrimitive = number | string | boolean | symbol | null | undefined;
+export type anyValue = object | anyPrimitive;
 export type anyObject_T<T> = { [key: string]: T };
 export type anyObject = anyObject_T<anyValue>;
